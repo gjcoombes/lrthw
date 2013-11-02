b86ac7e1bd1f3cdebaea9af03e89c7f7f8a4c570
Gavin Coombes <gjcoombes@gmail.com>
2013-11-02 21:06:48 +0800

    exercise 2

---
+++ ex1.rb
@@ -0,0 +1,6 @@
+# Learn Ruby the Hard Way
+
+puts "hello world"
+puts "this is fab"
+puts 'maybe singles work too'
+puts " but I would like to do something more than just type"
---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# Learn Ruby the Hard Way Ex2
+
+# Yes this is comment
+
+puts "And this is not a comment and so will be printed"
+
+
+# and this wil not be
